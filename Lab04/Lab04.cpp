ee16ba4d38689f78f11ec18c076d611bb89d5ccb
steims <102365392+steims@users.noreply.github.com>
2025-11-04 12:49:21 +0100

    Update Lab04.cpp

--- Lab04/Lab04.cpp
+++ Lab04/Lab04.cpp
@@ -167,30 +167,45 @@ double minimum(double a, double b) {
 
 int main()
 {
-    cout << "=== Function Practice Tasks ===" << endl;
+   cout << "=== Function Practice Tasks ===" << endl;
 
     // TASK 1 test
-     cout << "Sum of three: " << sum3(1.1, 2.2, 3.3) << endl;
-
-    // TASK 2 test
-     print_stars_advanced(5, '#');
-
-    // TASK 3 test
-     cout << "Factorial of 5: " << factorial(5) << endl;
-
-    // TASK 4 test
-     cout << "Estimated π: " << estimate_pi(10000) << endl;
-
-    // TASK 5 test
-     cout << "Minimum of 10 and 3.14: " << minimum(10, 3.14) << endl;
-     cout << "Minimum of 3.14 and 10: " << minimum(3.14, 10) << endl;
-     cout << "Minimum of 11 and 5: " << minimum(11, 5) << endl;
-     cout << "Minimum of 1 and 7.22: " << minimum(1, 7.22) << endl;
-
-    return 0;
+   cout << "Sum of three: " << sum3(1.1, 2.2, 3.3) << endl;
+   cout << "Sum of three: " << sum3(1.0, 2.0, 3.0) << endl;
+    
+   // TASK 2 test
+   
+   print_stars_advanced(5, '#');
+   print_stars_advanced(8, '*');
+   print_stars_advanced(2, '$');
+   
+   // TASK 3 test
+    
+   cout << "Factorial of 5: " << factorial(5) << endl;
+   cout << "Factorial of 1: " << factorial(1) << endl;
+   cout << "Factorial of 0: " << factorial(0) << endl;
+   cout << "Factorial of 7: " << factorial(7) << endl;
+    
+   // TASK 4 test
+    
+   cout << "Estimated π: " << estimate_pi(10000) << endl;
+   
+   // TASK 5 test
+   
+   cout << "Minimum of 10 and 3.14: " << minimum(10, 3.14) << endl;
+   cout << "Minimum of 3.14 and 10: " << minimum(3.14, 10) << endl;
+   cout << "Minimum of 11 and 5: " << minimum(11, 5) << endl;
+   cout << "Minimum of 1 and 7.22: " << minimum(1, 7.22) << endl;
+   cout << "Minimum of 5.3 and 2.1: " << minimum(5.3, 2.1) << endl;
+   cout << "Minimum of 4.5 and 8.14: " << minimum(4.5, 8.14) << endl;
+   cout << "Minimum of 76 and 34: " << minimum(76, 34) << endl;
+   cout << "Minimum of 45.78 and 87.98: " << minimum(45.78, 87.98) << endl;
+   
+   return 0;
 }
 
 /*
   End of file.
   Practice makes progress — not perfection.
+
 */

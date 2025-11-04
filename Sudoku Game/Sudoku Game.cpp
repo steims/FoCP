9ce45b6f554a5357cd614e2bcab841121fae5535
steims <102365392+steims@users.noreply.github.com>
2025-11-04 13:19:23 +0100

    Add files via upload

---
+++ Sudoku Game/Sudoku Game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sudoku[10][10];
+	for (int i = 1; i < 10; ++i)
+	{
+		for (int j = 1; j < 10; ++j)
+		{
+			sudoku[i][j] = rand() %9 + 1;
+			cout << sudoku[i][j] << "\t" << endl;
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
